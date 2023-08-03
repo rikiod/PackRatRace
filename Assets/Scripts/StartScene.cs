64fb625aec8617637b3ff35116fe4aa2480264b4
Rikio <rikio@berkeley.edu>
2023-08-03 11:18:16 +0200

    adding collider to start button. still need to add scene to build settings i think

--- Assets/Scripts/StartScene.cs
+++ Assets/Scripts/StartScene.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartScene : MonoBehaviour
 {
+
+    // make sure sprite is clickable / has collider 
+    [SerializeField] string sceneName = "Level1";
+
+    private void OnTriggerEnter(Collider other) {
+        LoadScene();
+    }
+    // private void OnMouseDown() {
+    //     print("hi");
+    //     LoadScene();
+    // }
+
+    private void LoadScene() {
+        SceneManager.LoadScene(sceneName);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("start");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    // // Update is called once per frame
+    // void Update()
+    // {
         
-    }
+    // }
 }
