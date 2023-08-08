8535524678593a03d95f19c02488eb027466bc4b
LyckuRe <91209764+LyckuRe@users.noreply.github.com>
2023-08-08 21:08:58 +0800

    bug fix

--- Assets/Scripts/BoxCloserScripts/CheckCollision.cs
+++ Assets/Scripts/BoxCloserScripts/CheckCollision.cs
@@ -72,19 +72,26 @@ public class CheckCollision : MonoBehaviour //needs to implement listener for on
             {
                 for (int i = detectedItems.Count - 1; i >= 0; i--)
                 {
-/*                    Debug.Log(detectedItems[i]);*/
-                    if (objectToDetect.Contains(detectedItems[i].name))
+                    // Debug.Log(detectedItems[i]);
+                    if (detectedItems[i] != null){
+                        if (objectToDetect.Contains(detectedItems[i].name))
                     {
                         counter[detectedItems[i].gameObject.name]++;
                     }
+                    }
+                    
                 }
                 for (int i = detectedItems.Count - 1; i >= 0; i--)
                 {
-                    Debug.Log(detectedItems[i]);
-                    if (objectToDetect.Contains(detectedItems[i].name) || detectedItems[i].name == "BoxOpen")
+                    // Debug.Log(detectedItems[i]);
+                    if (detectedItems[i] != null){
+                        if (objectToDetect.Contains(detectedItems[i].name) || detectedItems[i].name == "BoxOpen")
                     {
                         Destroy(detectedItems[i].gameObject);
                     }
+                    }
+                        
+                    
                 }
                 detectedItems.Clear();
             }
