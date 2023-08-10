639ea988659f95f7f79bf4d78b7c20933316b4ae
LyckuRe <91209764+LyckuRe@users.noreply.github.com>
2023-08-10 20:59:35 +0800

    stuff

--- Assets/Scripts/Level2Grinder/CanRecepticle.cs
+++ Assets/Scripts/Level2Grinder/CanRecepticle.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CanRecepticle : MonoBehaviour
 {
@@ -8,19 +9,28 @@ public class CanRecepticle : MonoBehaviour
     public GameObject correctCan;
     public GameObject incorrectCan;
     // public GameObject specialCan;
+    public UnityEvent goodCan;
+    public UnityEvent badCan;
 
     private void OnTriggerEnter(Collider other) {
         // print("collision !!!");
         // Debug.Log(other.gameObject);
 
         if (other.gameObject == correctCan) {
+            goodCan.Invoke();
             print("Can was correct!!!!");
         }
         else if (other.gameObject == incorrectCan) {
+            badCan.Invoke();
             print("Can was incorrect!! >:(");
         }
+        StartCoroutine(destroyCan(other.gameObject));
+    }
 
-        Destroy(other.gameObject);
+    private IEnumerator destroyCan(GameObject can)
+    {
+        yield return new WaitForSeconds(10);
+        Destroy(can);
     }
 
 }
