4ec2a1e11eb8390f84ffb3b5fbb33e8d7bd17a98
LyckuRe <91209764+LyckuRe@users.noreply.github.com>
2023-08-10 08:48:29 +0800

    big commit

--- Assets/Scripts/Level2Grinder/grinderButtonScript.cs
+++ Assets/Scripts/Level2Grinder/grinderButtonScript.cs
@@ -8,10 +8,12 @@ public class grinderButtonScript : MonoBehaviour
     public UnityEvent onRelease;
     private GameObject pusher;
     private bool isPressed;
+    private bool wasPressed;
     // Start is called before the first frame update
     void Start()
     {
         isPressed = false;
+        wasPressed = false;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -24,8 +26,19 @@ public class grinderButtonScript : MonoBehaviour
     {
         if (pusher == other.gameObject)
         {
-            onRelease.Invoke();
-            isPressed = false;
+            if (!wasPressed)
+            {
+                StartCoroutine(buttonPressed());
+                onRelease.Invoke();
+                isPressed = false;
+                wasPressed = true;
+            }
         }
+    }
+
+    private IEnumerator buttonPressed()
+    {
+        yield return new WaitForSeconds(6);
+        wasPressed = false;
     }
 }
